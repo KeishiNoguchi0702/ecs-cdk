5cf41f6dcf3c62984d940706fd721e39f0690f60
KeishiNoguchi0702 <thomastyaso1127@gmail.com>
2024-05-10 21:51:03 +0900

    :sparkles: VPCスタックを作成

--- lib/stacks/ecs-cdk-stack.ts
+++ lib/stacks/ecs-cdk-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { projectConstants } from '../constants/project-constants';
+import {
+  aws_ec2 as ec2,
+  aws_ssm as ssm,
+} from 'aws-cdk-lib';
 
 const projectName: string = projectConstants.projectName;
 const vpcName: string = projectConstants.vpcName;
@@ -8,5 +12,28 @@ const vpcName: string = projectConstants.vpcName;
 export class VpcStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, vpcName, {
+      vpcName: vpcName,
+      ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: `${projectName}-public-subnet`,
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: `${projectName}-private-subnet`,
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          cidrMask: 24,
+        }
+      ]
+    });
+
+    new ssm.StringParameter(this, 'vpc-id', {
+      parameterName: `/${projectName}/vpc-id`,
+      stringValue: vpc.vpcId,
+    });
   }
 }
